97e49b2bf9f9c098fe34bdb09a48234e49d1d1a7
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-06-25 22:54:04 +0100

    Clean Up

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,23 @@ import browserSync from 'browser-sync';
 
 const sass = gulpSass(dartSass);
 
+// paths
+var styleSrc = 'source/sass/**/*.sass',
+    styleDest = 'build/assets/css/',
+    htmlSrc = 'source/',
+    htmlDest = 'build/',
+    vendorSrc = 'source/js/vendors/',
+    vendorDest = 'build/assets/js/',
+    scriptSrc = 'source/js/*.js',
+    scriptDest = 'build/assets/js/';
+
 
 // --------------------------------------------
 // Stand Alone Tasks
 // --------------------------------------------
 
 // Compiles all SASS files
-gulp.task('sass', function() {
+gulp.task('sass', async function() {
     gulp.src('source/sass/**/*.sass')
     .pipe(sourcemaps.init())
         .pipe(plumber())
@@ -35,3 +45,62 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('build/assets/css'));
 });
 
+gulp.task('images', async function() {
+    gulp.src('source/img/*')
+        .pipe(images([
+            images.gifsicle({interlaced: true}),
+            images.mozjpeg({quality: 75, progressive: true}),
+            images.optipng({optimizationLevel: 5}),
+            images.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('build/assets/img'));
+});
+
+// Uglify js files
+gulp.task('scripts', async function() {
+    gulp.src('source/js/*.js')
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest('build/assets/js'));
+});
+
+
+//Concat and Compress Vendor .js files
+gulp.task('vendors', async function() {
+    gulp.src(
+            [
+                'source/js/vendors/jquery.min.js',
+                'source/js/vendors/*.js'
+            ], {allowEmpty: true})
+        .pipe(plumber())
+        .pipe(concat('vendors.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/assets/js'));
+});
+
+// Watch for changes
+gulp.task('watch', function(){
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        },
+        notify: false
+    });
+
+    gulp.watch(styleSrc,gulp.series('sass'));
+    gulp.watch(scriptSrc,gulp.series('scripts'));
+    gulp.watch(vendorSrc,gulp.series('vendors'));
+    gulp.watch(['build/*.html', 'build/assets/css/*.css', 'build/assets/js/*.js', 'build/assets/js/vendors/*.js']).on('change', browserSync.reload);
+
+});
+
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('default', gulp.parallel('sass', 'images', 'scripts', 'vendors'));
